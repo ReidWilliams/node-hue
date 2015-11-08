7c951a08d1ba738e6d3b8e772b07eab5396c6ca3
Reid Williams <rwilliams@ideo.com>
2015-11-08 13:11:13 -0800

    defining colors

---
+++ colors.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.blue = 255;
+exports.purple = 275;
+exports.pink = 300;
+exports.red = 0;
+exports.orange = 20;
+exports.yellowOrange = 40;
+exports.yellow = 70;
+exports.green = 140;
