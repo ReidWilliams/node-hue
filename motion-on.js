e5ba358706f1d3cbe6cc1599ea78541ca5636db6
Reid Williams <rwilliams@ideo.com>
2015-11-09 21:00:23 -0800

    changed timings

--- motion-on.js
+++ motion-on.js
@@ -7,11 +7,9 @@ var _ = require('underscore');
 
 var light = _.findWhere(constants.lights, {name: 'bookshelf'});
 
-var timeOff = 10000;
-
 var firstOn = lightState.create().on(true).hsb(250, 100, 0);
 var on = lightState.create().on(true).hsb(50, 50, 100).transition(3000);
-var low = lightState.create().on(true).hsb(250, 100, 0).transition(timeOff);
+var low = lightState.create().on(true).hsb(250, 100, 0).transition(10000);
 var off = lightState.create().on(false);
 
 var timerID = 0;
@@ -21,19 +19,18 @@ setInterval(function() {
 	particle.getMotion()
 	.then(function(reply) {
 		if (reply.result !== 0 && stateOn === false) {
+			clearTimer(timerID);
 			api.setLightState(light.id, firstOn);
 			stateOn = true;
 		}
 		if (reply.result !== 0 && stateOn === true) {
-			console.log('on');
 			api.setLightState(light.id, on);
 		}
 		if (reply.result === 0 && stateOn === true) {
-			console.log('off');
 			api.setLightState(light.id, low);
 			timerID = setTimeout(function() {
 				api.setLightState(light.id, off);
-			}, timeOff);
+			}, 30000);
 			stateOn = false;
 		}
 	})
