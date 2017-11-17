bdc96d96ddcd199a8c90feac4f1b30baa2f5b672
Reid Williams <rwilliams@ideo.com>
2017-11-16 19:45:03 -0800

    changed light id

--- constants.js
+++ constants.js
@@ -1,27 +1,26 @@
 'use strict';
 
 exports.lights = [
-	{
-		name: 'office-desk',
-		id: 9
-	},
-	{
-  	name: 'office-desk2',
-  	id: 7
-  },
-  {
-  	name: 'living-room-shelf',
-  	id: 3
-  },
-  {
-    name: 'front-entryway',
-    id: 8
-  },
   {
     name: 'bedroom',
     id: 2
+  },
+
+  {
+    name: 'living-room-shelf',
+    id: 3
+  },
+
+  {
+    name: 'front-entryway',
+    id: 6
+  },
+
+	{
+  	name: 'office-desk2',
+  	id: 7
   }
-  ];
+];
 
 exports.ip = '192.168.7.35';
 exports.username = '24f1532c2c510fef33c1930416a95b87';
--- light-status.js
+++ light-status.js
@@ -1,16 +1,16 @@
-var hue = require("node-hue-api");
-var constants = require('./constants');
-var api = new hue.HueApi(constants.ip, constants.username);
-var _ = require('underscore');
+var hue = require("node-hue-api")
+var constants = require('./constants')
+var api = new hue.HueApi(constants.ip, constants.username)
+var _ = require('underscore')
 
 _.each(constants.lights, function(light) {
 	api.lightStatus(light.id)
     .then(function(status) {
-		console.log("light name: " + light.name);
-		console.log("light id: " + light.id);
-		console.log("====================");
-		console.log(JSON.stringify(status, null, 2));
+		console.log(light.name)
+		console.log("id:", light.id)
+		console.log("on:", status.state.on)
+		console.log("")
 	})
-    .done();
+    .done()
 });
 
