db9fc850bf09444a89a16d3fac5e38aac04d37f9
Reid Williams <rwilliams@ideo.com>
2018-01-08 21:40:19 -0800

    uses hsb rather than white

--- repeatedly-set.js
+++ repeatedly-set.js
@@ -10,28 +10,28 @@ var lightState = hue.lightState;
 var _ = require('underscore');
 
 var main = function() {
-	if (process.argv.length < 6) {
+	if (process.argv.length < 7) {
 		usage();
 		process.exit(0);
 	}
 
 	var light = _.findWhere(constants.lights, {name: process.argv[2]});
-	var color = process.argv[3];
-	var brightness = parseInt(process.argv[4]);
-	var seconds = parseInt(process.argv[5]);
+	var hue = process.argv[3];
+	var sat = process.argv[4]
+	var brightness = parseInt(process.argv[5]);
+	var seconds = parseInt(process.argv[6]);
 
 	setInterval(function() {
-		var ls = lightState.create().on(true).white(color, brightness);
+		var ls = lightState.create().on(true).hsb(hue, sat, brightness);
 		api.setLightState(light.id, ls);
 	}, seconds * 1000);
 }
 
 var usage = function() {
-	console.log("usage: node " + __filename + " LIGHTNAME COLOR BRIGHTNESS SECONDS");
+	console.log("usage: node " + __filename + " LIGHTNAME HUE SAT BRIGHTNESS SECONDS");
 	console.log("LIGHTNAME is the name of a light defined in constants.js:");
 	var lightNames = _.pluck(constants.lights, 'name').join(", ");
 	console.log('"' + lightNames + '"');
-	console.log("COLOR is a value between 154 (cool) and 500 (warm)");
 	console.log("SECONDS is how often to try setting the light to the given color/brightness")
 }
 
