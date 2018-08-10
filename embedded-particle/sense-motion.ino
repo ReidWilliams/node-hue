50a5cdb5294449b3dfbd3c78fbfd9acd0eed3025
Reid Williams <rwilliams@ideo.com>
2018-08-09 22:35:59 -0700

    sensor more sensitive

--- embedded-particle/sense-motion.ino
+++ embedded-particle/sense-motion.ino
@@ -34,7 +34,7 @@ const int kVcc = D2;
 
 // tracks how many PIR reads have been high in a row
 // 10 is too sensitive, 20 works ok
-const int onThreshold = 20;
+const int onThreshold = 18;
 const int maxOn = 180;
 int highReads = 0;
 
